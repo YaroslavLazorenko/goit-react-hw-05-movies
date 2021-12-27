e147ca3f47411681277f08a460c912d9c50723cf
YaroslavLazorenko <lazorenko85@gmail.com>
2021-12-27 17:44:37 +0200

    Add some routes and components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import AppBar from './components/AppBar';
 import HomePage from './views/HomePage';
+import MoviesPage from './views/MoviesPage';
+import MovieDetailsPage from './views/MovieDetailsPage';
 import './App.css';
 
 function App() {
   return (
     <>
+      <AppBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        {/* <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        {/* <Route path="/movies/:movieId/cast" element={<Cast />} />
         <Route path="/movies/:movieId/reviews" element={<Reviews />} /> */}
+        {/* <NavigateToHomePage /> */}
+        <Route path="/*" element={<Navigate to="/" replace />} />
       </Routes>
     </>
   );
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,7 +1,16 @@
+import { useState, useEffect } from 'react';
+import fetchMovies from '../services/themoviedb-api';
+
 export default function HomeView() {
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  useEffect(() => {
+    setTrendingMovies(fetchMovies.trendingMovies());
+  }, []);
+
   return (
     <>
-      <h1>Home</h1>
+      <h1>Trending today</h1>
+      <ul></ul>
     </>
   );
 }
